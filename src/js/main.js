b1f03694ba29ad6abfaac3019f5a58f06d7e8918
Mara Rocha Fernández <maranhadisenha@gmail.com>
2021-12-28 18:35:11 +0100

    get data from api and listen search button

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,58 @@
 'use strict';
 
-/* Do your magic! 🦄🦄🦄 */
+
+/* Let's do magic! 🦄🦄🦄 */
+
+
+// global data
+
+const API_URL = 'https://api.jikan.moe/v3/search/anime';
+
+let searchTerm = 'naruto';
+
+let animeSeries = [];
+let favorites = [];
+
+
+// render anime series
+
+function renderAnimeSeries() {
+  // render anime series
+}
+
+
+// api
+
+function getAnimeSeriesFromApi() {
+
+  // api documentation: only processes queries with a minimum of 3 letters
+  if (searchTerm.length >= 3) {
+    fetch(`${API_URL}?q=${searchTerm}`)
+      .then(response => response.json())
+      .then(data => {
+        animeSeries = data.results;
+        renderAnimeSeries();
+      });
+  }
+}
+
+
+// listen and handle search button event
+
+function listenSearchButtonEvent() {
+  const searchButtonElement = document.querySelector('.js-button-submit');
+  searchButtonElement.addEventListener('click', handleSearchButtonEvent);
+}
+
+function handleSearchButtonEvent(event) {
+  event.preventDefault();
+  const searchTermElement = document.querySelector('.js-search-term');
+  searchTerm = searchTermElement.value ? searchTermElement.value : searchTerm;
+  getAnimeSeriesFromApi();
+}
+
+
+// start app
+
+getAnimeSeriesFromApi();
+listenSearchButtonEvent();
